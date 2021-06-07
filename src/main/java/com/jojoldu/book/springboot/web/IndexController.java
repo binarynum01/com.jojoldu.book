32b8a5682d880ff079f9fd1a9378e6f935f2fff1
BinaryNum <jinsu94612@gmail.com>
2021-06-07 16:20:07 +0900

    게시판 등록화면

---
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -0,0 +1,14 @@
+package com.jojoldu.book.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class IndexController {
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+}
